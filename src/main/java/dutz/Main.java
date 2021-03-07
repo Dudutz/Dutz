72504e09c983c68ba802986124afd11af0b2353d
Dudutz <maierluizeduardo@gmail.com>
2021-03-07 13:58:01 -0300

    Primeiro commit.

---
+++ src/main/java/dutz/Main.java
@@ -0,0 +1,12 @@
+package dutz;
+
+public class Main {
+	public static void main(String[] arguments){
+	Professor presunto = new Professor (Integer.MIN_VALUE, "presunto");
+	Professor indjara = new Professor( 107, "indjara");
+	Professor elis = new Professor(Integer.MAX_VALUE, "elis");
+	Professor cleitinCrispinJose = new Professor(95, "cleitin crispin jose");
+	Professor ph = new Professor(115, "ph");
+	
+	}
+	
---
+++ src/main/java/dutz/Professor.java
@@ -0,0 +1,10 @@
+package dutz;
+
+public class Professor {
+	int QI = 0;
+	String nome = "nome";
+	public Professor(int QI, String nome) {
+		this.QI = QI;
+		this.nome = nome;
+	}
+}
